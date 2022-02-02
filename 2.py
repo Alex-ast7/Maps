6a6ff4d1320e9c1ba21c1409832f6d5417779461
julia <jufedorishcheva@gmail.com>
2022-02-02 15:10:29 +0400

    пробное решение задачи 2

---
+++ 2.py
@@ -0,0 +1,64 @@
+import sys
+
+from PIL import Image, ImageQt
+from io import BytesIO
+import requests
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtCore import Qt
+
+SCREEN_SIZE = [600, 450]
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.lon, self.lat = input().split(', ')
+        self.spn = float(input())
+        self.getImage()
+        self.initUI()
+
+    def getImage(self):
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={self.lon}," \
+                      f"{self.lat}8&spn={str(self.spn)},{str(self.spn)}&l=map"
+        response = requests.get(map_request)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request)
+            print("Http статус:", response.status_code, "(", response.reason,
+                  ")")
+            sys.exit(1)
+
+        self.img = ImageQt.ImageQt(Image.open(BytesIO(response.content)))
+
+    def initUI(self):
+        self.setGeometry(100, 100, *SCREEN_SIZE)
+        self.setWindowTitle('Отображение карты')
+
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(600, 450)
+        self.image.setPixmap(QPixmap.fromImage(self.img))
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            self.spn += 0.001
+            print(self.spn)
+        elif event.key() == Qt.Key_PageDown:
+            self.spn -= 0.001
+            print(self.spn)
+        self.getImage()
+        self.initUI()
+
+
+
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
+# 46.049690, 51.527601
