291a9abbda6ca580c143d6408546ac1e4cee65cd
Alex-ast7 <alex7ast@gmail.com>
2022-02-05 20:44:16 +0400

    ввод параметров через приложение

--- 1.py
+++ 1.py
@@ -3,19 +3,45 @@ import sys
 from PIL import Image, ImageQt
 from io import BytesIO
 import requests
+from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, \
+    QPushButton
 
-SCREEN_SIZE = [600, 450]
+SCREEN_SIZE = [600, 550]
 
 
 class Example(QWidget):
     def __init__(self):
         super().__init__()
-        self.lon, self.lat = input().split(', ')
-        self.spn = input()
-        self.getImage()
-        self.initUI()
+        self.setObjectName("Отображение карты")
+        self.setGeometry(100, 100, *SCREEN_SIZE)
+        self.ll_text = QtWidgets.QLabel(self)
+        self.ll_text.setText('Введите координаты:')
+        self.ll_text.move(10, 460)
+        self.ll_text.resize(110, 20)
+        self.ll_text.setGeometry(QtCore.QRect(10, 460, 110, 20))
+        self.ll_text = QtWidgets.QLabel(self)
+        self.ll_text.setText('Введите координаты:')
+        self.ll_text.setGeometry(QtCore.QRect(10, 460, 110, 20))
+        self.ll_edit = QtWidgets.QLineEdit(self)
+        self.ll_edit.move(130, 460)
+        self.ll_edit.resize(100, 20)
+        self.spn_text = QtWidgets.QLabel(self)
+        self.spn_text.setText('Введите масштаб:')
+        self.spn_text.move(10, 490)
+        self.spn_text.resize(110, 20)
+        self.spn_edit = QtWidgets.QLineEdit(self)
+        self.spn_edit.move(130, 490)
+        self.spn_edit.resize(100, 20)
+        self.start_show = QtWidgets.QPushButton(self)
+        self.start_show.setText('Показать карту')
+        self.start_show.move(260, 510)
+        self.start_show.resize(120, 30)
+        self.start_show.clicked.connect(self.start)
+        self.image = QtWidgets.QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(600, 450)
 
     def getImage(self):
         map_request = f"http://static-maps.yandex.ru/1.x/?ll={self.lon}," \
@@ -32,14 +58,16 @@ class Example(QWidget):
         self.img = ImageQt.ImageQt(Image.open(BytesIO(response.content)))
 
     def initUI(self):
-        self.setGeometry(100, 100, *SCREEN_SIZE)
-        self.setWindowTitle('Отображение карты')
-
-        self.image = QLabel(self)
-        self.image.move(0, 0)
-        self.image.resize(600, 450)
         self.image.setPixmap(QPixmap.fromImage(self.img))
 
+    def start(self):
+        try:
+            self.lon, self.lat = self.ll_edit.text().split(', ')
+            self.spn = self.spn_edit.text()
+            self.getImage()
+            self.initUI()
+        except Exception:
+            print('Недопустимое значение параметров')
 
 
 if __name__ == '__main__':
@@ -47,3 +75,5 @@ if __name__ == '__main__':
     ex = Example()
     ex.show()
     sys.exit(app.exec())
+# 46.134068, 51.490845
+# 0.001
