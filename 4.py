efaa56657c92767c87cb4fa7f35d0d3047c6665a
julia <jufedorishcheva@gmail.com>
2022-02-06 13:30:18 +0400

    решение 4 задачи

--- 4.py
+++ 4.py
@@ -1,7 +1,6 @@
+import os
 import sys
 
-from PIL import Image, ImageQt
-from io import BytesIO
 import requests
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtGui import QPixmap
@@ -47,67 +46,81 @@ class Example(QWidget):
         self.map_text.setText('Введите слой карты:')
         self.map_text.move(210, 460)
         self.map_text.resize(110, 20)
-        self.map_text.setGeometry(QtCore.QRect(10, 460, 110, 20))
-        self.map_text = QtWidgets.QLabel(self)
-        self.map_text.setText('Введите координаты:')
         self.map_text.setGeometry(QtCore.QRect(210, 460, 110, 20))
         self.map_edit = QtWidgets.QLineEdit(self)
         self.map_edit.move(330, 460)
         self.map_edit.resize(100, 20)
 
-
     def getImage(self):
         map_request = f"http://static-maps.yandex.ru/1.x/?ll={self.lon}," \
-                      f"{self.lat}8&spn={str(self.spn)},{str(self.spn)}&l={self.map_type}"
+                      f"{self.lat}8&spn={str(self.spn)},{str(self.spn)}&l=" \
+                      f"{self.map_type}"
+        print(map_request)
         try:
             response = requests.get(map_request)
-            self.img = Image.open(BytesIO(response.content))
+            self.map_file = "map.png"
+            with open(self.map_file, "wb") as file:
+                file.write(response.content)
             self.update_image()
         except Exception:
             print('недопустимое значение масштаба')
 
-
     def update_image(self):
-        self.image.setPixmap(QPixmap.fromImage(ImageQt.ImageQt(self.img)))
-
+        try:
+            self.pixmap = QPixmap(self.map_file)
+            self.image.setPixmap(self.pixmap)
+        except Exception as e:
+            print(e)
 
     def keyPressEvent(self, event):
-        print('sddsdsdsa')
         if event.key() == Qt.Key_PageUp:
             self.spn += 0.001
+            self.spn_edit.setText(str(self.spn))
             self.getImage()
         elif event.key() == Qt.Key_PageDown:
             self.spn -= 0.001
+            self.spn_edit.setText(str(self.spn))
             self.getImage()
         elif event.key() == Qt.Key_W:
             self.lat += 0.001
+            self.ll_edit.setText(f"{self.lon}, {self.lat}")
             self.getImage()
         elif event.key() == Qt.Key_S:
             self.lat -= 0.001
+            self.ll_edit.setText(f"{self.lon}, {self.lat}")
             self.getImage()
         elif event.key() == Qt.Key_D:
             self.lon += 0.001
+            self.ll_edit.setText(f"{self.lon}, {self.lat}")
             self.getImage()
         elif event.key() == Qt.Key_A:
             self.lon -= 0.001
+            self.ll_edit.setText(f"{self.lon}, {self.lat}")
             self.getImage()
         # сделали на кнопки WASD, потому что по нажатию на стрелочки функция
         # не срабатывает, а передвигается курсор в line_edit
-
 
     def start(self):
         try:
             self.lon, self.lat = map(float, self.ll_edit.text().split(', '))
             self.spn = float(self.spn_edit.text())
-            self.map_type = self.map_text.text()
+            self.map_type = self.map_edit.text()
             self.getImage()
             self.update_image()
+            self.ll_edit.setEnabled(False)
+            self.spn_edit.setEnabled(False)
         except Exception:
             print('Недопустимое значение параметров')
 
+    def closeEvent(self, event):
+        os.remove(self.map_file)
+
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Example()
-    ex.show()
-    sys.exit(app.exec())
+    try:
+        app = QApplication(sys.argv)
+        ex = Example()
+        ex.show()
+        sys.exit(app.exec())
+    except Exception as e:
+        print(e)
